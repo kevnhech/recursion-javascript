d69fab3ca1e1e3789afbaf6cbb1b3a4a27543112
kevin <kevnhech@gmail.com>
2024-12-03 11:58:15 -0800

    Solve fibonacci problem with iteration

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+// Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the Fibonacci sequence. Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+
+function fibs(n) {
+  let arr = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i < 2) {
+      arr.push(i);
+    } else {
+      arr.push(arr[i - 1] + arr[i - 2]);
+    }
+  }
+
+  return arr;
+}
+
+console.log(fibs(8));
